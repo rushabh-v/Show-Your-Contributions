d879cb14c277929894d09a9666d0c71a1fdf9714
rushabh-v <vasanirushabh24@gmail.com>
2021-01-02 21:06:51 +0530

    run black (format code)

--- fetch_contribs.py
+++ fetch_contribs.py
@@ -5,61 +5,92 @@ from datetime import datetime
 import json
 
 repo = {
-        'stars': 0,
-        'lang': None,
-        'n_merged': 0,
-        'n_closed': 0,
-        'n_open': 0,
-        'last_mod': float('inf'),
-        'merged_url': None,
-        'open_url': None,
-        'closed_url': None,
-        }
+    "stars": 0,
+    "lang": None,
+    "n_merged": 0,
+    "n_closed": 0,
+    "n_open": 0,
+    "last_mod": float("inf"),
+    "merged_url": None,
+    "open_url": None,
+    "closed_url": None,
+}
 
 
 def creat_json(g, prs):
     my_contribs = {}
     user = g.get_user()
     issues = None
-    name = 'issue'
-    if prs: issues = g.search_issues(query="author:{} is:pr".format(user.login))
-    else:   issues = g.search_issues(query="author:{} is:issue".format(user.login))
+    name = "issue"
+    if prs:
+        issues = g.search_issues(query="author:{} is:pr".format(user.login))
+    else:
+        issues = g.search_issues(query="author:{} is:issue".format(user.login))
     n_issues = issues.totalCount
     repo_name = None
     for abc in issues:
         if prs:
             abc = abc.as_pull_request()
-            name = 'pr'
-            repo_name = abc.url[abc.url.index('repos')+6 : abc.url.index('pulls')-1]
-        else: repo_name = abc.url[abc.url.index('repos')+6 : abc.url.index('issues')-1]
+            name = "pr"
+            repo_name = abc.url[abc.url.index("repos") + 6 : abc.url.index("pulls") - 1]
+        else:
+            repo_name = abc.url[
+                abc.url.index("repos") + 6 : abc.url.index("issues") - 1
+            ]
         if repo_name not in my_contribs:
             my_contribs[repo_name] = repo.copy()
             r = g.get_repo(repo_name)
-            my_contribs[repo_name]['stars'] = r.stargazers_count
+            my_contribs[repo_name]["stars"] = r.stargazers_count
 
             repo_langs = list(r.get_languages().keys())
             if len(repo_langs) > 0:
-                my_contribs[repo_name]['lang'] = repo_langs[0]
+                my_contribs[repo_name]["lang"] = repo_langs[0]
             else:
-                my_contribs[repo_name]['lang'] = 'GitHub'
+                my_contribs[repo_name]["lang"] = "GitHub"
 
-            org, repo_ = repo_name.split('/')
-            if prs: my_contribs[repo_name]['merged_url'] = (
-                        'https://github.com/search?q=is%3Apr+repo%3A'+org+'%2F'+repo_+'+author%3A{}+is%3Amerged'.format(user.login))
-            my_contribs[repo_name]['closed_url'] = (
-            'https://github.com/search?q=is%3A'+name+'+repo%3A'+org+'%2F'+repo_+'+author%3A{}+is%3Aclosed'.format(user.login))
-            my_contribs[repo_name]['open_url'] = (
-            'https://github.com/search?q=is%3A'+name+'+repo%3A'+org+'%2F'+repo_+'+author%3A{}+is%3Aopen'.format(user.login))
-        if prs and abc.merged: my_contribs[repo_name]['n_merged'] += 1
-        elif abc.state == 'closed': my_contribs[repo_name]['n_closed'] += 1
-        else: my_contribs[repo_name]['n_open'] += 1
-        my_contribs[repo_name]['last_mod'] = min(my_contribs[repo_name]['last_mod'], (datetime.today() - abc.updated_at).days)
+            org, repo_ = repo_name.split("/")
+            if prs:
+                my_contribs[repo_name]["merged_url"] = (
+                    "https://github.com/search?q=is%3Apr+repo%3A"
+                    + org
+                    + "%2F"
+                    + repo_
+                    + "+author%3A{}+is%3Amerged".format(user.login)
+                )
+            my_contribs[repo_name]["closed_url"] = (
+                "https://github.com/search?q=is%3A"
+                + name
+                + "+repo%3A"
+                + org
+                + "%2F"
+                + repo_
+                + "+author%3A{}+is%3Aclosed".format(user.login)
+            )
+            my_contribs[repo_name]["open_url"] = (
+                "https://github.com/search?q=is%3A"
+                + name
+                + "+repo%3A"
+                + org
+                + "%2F"
+                + repo_
+                + "+author%3A{}+is%3Aopen".format(user.login)
+            )
+        if prs and abc.merged:
+            my_contribs[repo_name]["n_merged"] += 1
+        elif abc.state == "closed":
+            my_contribs[repo_name]["n_closed"] += 1
+        else:
+            my_contribs[repo_name]["n_open"] += 1
+        my_contribs[repo_name]["last_mod"] = min(
+            my_contribs[repo_name]["last_mod"], (datetime.today() - abc.updated_at).days
+        )
     return my_contribs, n_issues
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     g = Github(sys.argv[1])
     pulls, n_pulls = creat_json(g, True)
     issues, n_issues = creat_json(g, prs=False)
-    my_contribs = json.dumps({'pulls':pulls, 'issues':issues}, indent=4)
-    with open('my_contribs.json', 'w') as json_file:
+    my_contribs = json.dumps({"pulls": pulls, "issues": issues}, indent=4)
+    with open("my_contribs.json", "w") as json_file:
         json.dump(my_contribs, json_file)
--- generate_doc.py
+++ generate_doc.py
@@ -9,53 +9,60 @@ g = Github(sys.argv[1])
 user = g.get_user()
 cur_contrib = 0
 
-with open('templates.json', 'r') as j:
+with open("templates.json", "r") as j:
     templates = json.load(j)
 
 start = templates["start"].format(
     user.login,
     user.name if user.name is not None else user.login,
     user.avatar_url,
-    user.name if user.name is not None else user.login
+    user.name if user.name is not None else user.login,
 )
 middle = templates["middle"]
 tail = templates["tail"]
 
-with open('colors.json', 'r') as j:
+with open("colors.json", "r") as j:
     lang_colors = json.load(j)
 
-if isfile('total_contribs'):
-    with open('total_contribs', 'r') as f:
-        try: prev_contrib = int(f.read())
-        except: prev_contrib = -1
-else: prev_contrib = -1
+if isfile("total_contribs"):
+    with open("total_contribs", "r") as f:
+        try:
+            prev_contrib = int(f.read())
+        except:
+            prev_contrib = -1
+else:
+    prev_contrib = -1
 
 
 def add_row(contribs, key, is_pr):
-    org, repo_name = key.split('/')
-    merged_link = contribs[key]['merged_url']
-    open_link = contribs[key]['open_url']
-    closed_link = contribs[key]['closed_url']
-    n_merged = contribs[key]['n_merged']
-    n_open = contribs[key]['n_open']
-    n_closed = contribs[key]['n_closed']
-    lang = contribs[key]['lang']
-    n_days = contribs[key]['last_mod']
-    stars = contribs[key]['stars']
-    last_mod = ''
-    if n_days > 30: last_mod = str(n_days // 30) + " months ago"
-    else: last_mod = str(n_days) + " days ago"
+    org, repo_name = key.split("/")
+    merged_link = contribs[key]["merged_url"]
+    open_link = contribs[key]["open_url"]
+    closed_link = contribs[key]["closed_url"]
+    n_merged = contribs[key]["n_merged"]
+    n_open = contribs[key]["n_open"]
+    n_closed = contribs[key]["n_closed"]
+    lang = contribs[key]["lang"]
+    n_days = contribs[key]["last_mod"]
+    stars = contribs[key]["stars"]
+    last_mod = ""
+    if n_days > 30:
+        last_mod = str(n_days // 30) + " months ago"
+    else:
+        last_mod = str(n_days) + " days ago"
 
     if is_pr and (n_merged + n_open) == 0:
         return "", (n_merged + n_open + n_closed)
 
-    lang_color = (lang_colors[lang] if lang in lang_colors
-                     else 'rgb(0,255,0)')
+    lang_color = lang_colors[lang] if lang in lang_colors else "rgb(0,255,0)"
 
     repo_html = templates["pr_head"].format(org, repo_name, org, repo_name)
-    if n_merged: repo_html += templates["pr_merged"].format(merged_link, n_merged)
-    if n_open:   repo_html += templates["pr_open"].format(open_link, n_open)
-    if n_closed: repo_html += templates["pr_closed"].format(closed_link, n_closed)
+    if n_merged:
+        repo_html += templates["pr_merged"].format(merged_link, n_merged)
+    if n_open:
+        repo_html += templates["pr_open"].format(open_link, n_open)
+    if n_closed:
+        repo_html += templates["pr_closed"].format(closed_link, n_closed)
     repo_html += templates["pr_tail"].format(lang_color, lang, stars, last_mod)
 
     return repo_html, (n_merged + n_open + n_closed)
@@ -79,19 +86,21 @@ def generate_readme_image(readme_prs, readme_pr_keys):
     imgkit.from_string(html, "contributions.png", options=options)
 
 
-if __name__ == '__main__':
-    with open('my_contribs.json', 'r') as j:
+if __name__ == "__main__":
+    with open("my_contribs.json", "r") as j:
         contribs = json.load(j)
     contribs = json.loads(contribs)
-    try:    threshold = int(sys.argv[2])
-    except: threshold = 0
+    try:
+        threshold = int(sys.argv[2])
+    except:
+        threshold = 0
 
-    issues = contribs['issues']
-    prs = contribs['pulls']
+    issues = contribs["issues"]
+    prs = contribs["pulls"]
     readme_prs = remove_prs_below_threshold(prs, threshold)
 
-    get_count_pr = lambda d: (prs[d]['n_open'] + prs[d]['n_merged'])
-    get_count_issue = lambda d: (issues[d]['n_open'] + issues[d]['n_closed'])
+    get_count_pr = lambda d: (prs[d]["n_open"] + prs[d]["n_merged"])
+    get_count_issue = lambda d: (issues[d]["n_open"] + issues[d]["n_closed"])
 
     pr_keys = sorted(prs.keys(), key=get_count_pr, reverse=True)
     issues_keys = sorted(issues.keys(), key=get_count_issue, reverse=True)
@@ -115,7 +124,7 @@ if __name__ == '__main__':
     if prev_contrib == cur_contrib:
         exit()
 
-    with open('total_contribs', 'w') as f:
+    with open("total_contribs", "w") as f:
         f.write(str(cur_contrib))
-    with open('contributions.html', 'w') as f:
+    with open("contributions.html", "w") as f:
         f.write(html)
